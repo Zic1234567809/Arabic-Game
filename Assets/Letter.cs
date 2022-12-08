6902e768e5e59de6c45a948a03bc3731a6184f1d
ipb933 <93120398+ipb933@users.noreply.github.com>
2022-12-08 15:38:07 +0200

    Add files via upload

--- Assets/Letter.cs
+++ Assets/Letter.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Letter : MonoBehaviour{
+    public GameObject GrabHelper;
+    public GameObject LetterPlace;
+    public GameObject LetterPlace2;
+    public GameObject LetterPlace3;
+    private int x;
     void Start(){
         
     }
     void Update(){
-        
+        if (x == 1){
+            transform.position = (Vector3)GrabHelper.transform.position;
+            transform.rotation = GrabHelper.transform.rotation;
+        }
+        if (GrabHelper.activeInHierarchy == false){
+            x = 0;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.layer == 7 && GrabHelper.activeInHierarchy == true){
+            x = 1;
+        }if (other.gameObject.layer == 8){
+            x = 0;
+            transform.position = (Vector3)LetterPlace.transform.position;
+            transform.rotation = LetterPlace.transform.rotation;
+        }if (other.gameObject.layer == 9){
+            x = 0;
+            transform.position = (Vector3)LetterPlace2.transform.position;
+            transform.rotation = LetterPlace2.transform.rotation;
+        }if (other.gameObject.layer == 10){
+            x = 0;
+            transform.position = (Vector3)LetterPlace3.transform.position;
+            transform.rotation = LetterPlace3.transform.rotation;
+        }
     }
 }
--- Assets/player.cs
+++ Assets/player.cs
@@ -8,14 +8,15 @@ public class player : MonoBehaviour
     float horizontal;
     float vertical;
     public float RunSpeed;
-    public Transform Gun;
+    public GameObject Gun;
     public GameObject Bullet;
     public float BulletSpeed;
     public float bulletCooldown;
     float bulletCountdown;
-    public Transform ShootPoint;
+    public GameObject ShootPoint;
     public GameObject Grab1;
     public GameObject Grab2;
+    public GameObject GrabHelper;
     Vector2 direction;
     void Start(){
         body = GetComponent<Rigidbody2D>();
@@ -26,17 +27,21 @@ public class player : MonoBehaviour
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         direction = mousePos - (Vector2)transform.position;
         transform.right = direction;
-        if (Input.GetMouseButtonDown(0) && bulletCountdown <= 0){
-            GameObject BulletInstantiate = Instantiate(Bullet, ShootPoint.position, ShootPoint.rotation);
+        if (Input.GetMouseButtonDown(0) && bulletCountdown <= 0 && Gun.activeInHierarchy == true){
+            GameObject BulletInstantiate = Instantiate(Bullet, ShootPoint.transform.position, ShootPoint.transform.rotation);
             BulletInstantiate.GetComponent<Rigidbody2D>().AddForce(BulletInstantiate.transform.right * BulletSpeed);
             bulletCountdown = bulletCooldown;
         }
         if (Input.GetMouseButtonDown(1)){
             Grab1.SetActive(true);
             Grab2.SetActive(true);
+            GrabHelper.SetActive(true);
+            Gun.SetActive(false);
         }else if (Input.GetMouseButtonUp(1)){
             Grab1.SetActive(false);
             Grab2.SetActive(false);
+            GrabHelper.SetActive(false);
+            Gun.SetActive(true);
         }
         bulletCountdown -= Time.deltaTime;
     }
