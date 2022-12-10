f4e1eb87377409ef957645c40c8ac133fc83a93e
Electrified <elad.zic@gmail.com>
2022-12-10 17:34:41 +0200

    Added Enemy Movement

--- Assets/Letter.cs
+++ Assets/Letter.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Letter : MonoBehaviour{
     public GameObject GrabHelper;
     public static int x;
-    void Start(){
-        
-    }
+
+    public float movementRange;
+    private Vector3 desiredPos;
+    public float speed;
+    private bool isMoving;
+    public float minDistance;
+    public Transform topleft;
+    public Transform bottomright;
+
     void Update(){
+        StartCoroutine(Move());
+
         if (x == 1){
             transform.position = (Vector3)GrabHelper.transform.position;
             transform.rotation = GrabHelper.transform.rotation;
@@ -20,6 +28,28 @@ public class Letter : MonoBehaviour{
     private void OnTriggerEnter2D(Collider2D other){
         if (other.gameObject.layer == 7 && GrabHelper.activeInHierarchy == true){
             x = 1;
+        }
+    }
+
+    private IEnumerator Move()
+    {
+        if(!isMoving)
+        {
+            isMoving = true;
+            while(true)
+            {
+                desiredPos = new Vector3(Random.Range(-movementRange,movementRange), Random.Range(-movementRange,movementRange), 0) + transform.position;
+                if(topleft.position.x < desiredPos.x && desiredPos.x < bottomright.position.x && bottomright.position.y < desiredPos.y && desiredPos.y < topleft.position.y
+                 && Vector3.Distance(transform.position, desiredPos) > minDistance)
+                    break;
+            }
+        }
+        
+        transform.position = Vector3.MoveTowards(transform.position, desiredPos, speed * Time.deltaTime);
+        if(Vector3.Distance(transform.position, desiredPos) < 1)
+        {
+            yield return new WaitForSeconds(1);
+            isMoving = false;
         }
     }
 }
